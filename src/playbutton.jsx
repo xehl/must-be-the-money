42c7f0535dd24c35293dd3c6e30677f0746f3fda
ehlee95 <eric95lee@gmail.com>
2022-10-17 13:00:25 -0400

    mobile responsive

--- src/playbutton.jsx
+++ src/playbutton.jsx
@@ -1,19 +1,19 @@
 export default function PlayButton({ playing, handleClick }) {
   return (
-    <>
+    <div>
       <img
-        className={playing ? "show" : "hide"}
+        className={playing ? "play-button" : "hide"}
         onClick={handleClick}
         src={require("./media/frame1.png")}
         alt="big red button"
       />
 
       <img
-        className={playing ? "hide" : "show"}
+        className={playing ? "hide" : "play-button"}
         onClick={handleClick}
         src={require("./media/frame2.png")}
         alt="big red button"
       />
-    </>
+    </div>
   )
 }
